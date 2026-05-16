197d5dd3054e93e35da830234ba6532085c87ddc
Shaxina25 <shaxzodsobitaliev9@gmail.com>
2026-05-16 12:37:49 +0500

    remove desktop layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import "../css/globals.css";
 import type { ReactNode } from "react";
-import { Navigation } from "@/layouts/navigation";
-import NextTopLoader from 'nextjs-toploader';
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
-
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export default function RootLayout({
   children,
@@ -15,9 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru" className={cn("font-sans", geist.variable)}>
-      <body className="bg-[#f3f3f3] text-[#1e1e1e]">
-        <NextTopLoader />
-        <Navigation />
+      <body className="bg-[#050b25] text-white">
         {children}
       </body>
     </html>
